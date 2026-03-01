c8e2dfe0b0e6f6c5e9bac4c70a01a7903951d22e
anshdholakiya <dholakiyaansh1707@gmail.com>
2026-03-01 16:08:34 +0530

    authmiddleware some code updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.protect = async (req, res, next) => {
   let token;
-
+   
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
